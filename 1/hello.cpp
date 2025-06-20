db0fb72e165c4e8ce1b09cc49194000ae2a404be
zenoe <2004lzy156@163.com>
2025-06-20 21:33:12 +0800

    ....

--- 1/hello.cpp
+++ 1/hello.cpp
@@ -1,14 +1,17 @@
-// #include "hello.h"
+#include "../tool.h"
+#include "pt.h"
 #include <iostream>
 #include <limits>
 #include <vector>
-#include "pt.h"
-#include "../tool.h"
+#include <algorithm>  // min_element
+#include <memory>
+#include "Counter.h"
+#include "fraction.h"
 
 using ivec = std::vector<int>;
-void printp2dv(const std::vector<p2d> &p2dv){
-  for(const auto i : p2dv){
-    std::cout << "(" << i.x << "," << i.y  << ")";
+void printp2dv(const std::vector<p2d> &p2dv) {
+  for (const auto i : p2dv) {
+    std::cout << "(" << i.x << "," << i.y << ")";
   }
   print();
 }
@@ -46,7 +49,11 @@ template <typename T> void printVectorT(const std::vector<T> &v) {
 }
 
 void testvector() {
-  ivec v{1, 24, 6, 9};
+  ivec v{1, 24, -1, 6, 9};
+  // distance returns the size of an iterator range
+  print("distance", distance(v.begin(), v.end()));
+  auto argmin = distance(begin(v), min_element(begin(v),end(v)) );
+  print("argmin", argmin);  // 2
   for (auto i = rbegin(v); i != rend(v); ++i) {
     std::cout << *i << ' ';
   }
@@ -75,57 +82,59 @@ void testvector() {
   i2 = v3.insert(i2, 8); // i2 pt to 8
   printVectorT(v3);
   std::cout << "i2: " << *i2 << "\n";
-  i2 = v3.insert(begin(v3), {77,88, 99});  // i2 pt to 77
+  i2 = v3.insert(begin(v3), {77, 88, 99}); // i2 pt to 77
   printVectorT(v3);
   std::cout << "i2: " << *i2 << "\n";
-  i2 = v3.insert(end(v3)-1, 22);
-  std::cout << "i2: " << *i2 << " " << *( i2+1 ) <<  "\n";
+  i2 = v3.insert(end(v3) - 1, 22);
+  std::cout << "i2: " << *i2 << " " << *(i2 + 1) << "\n";
   printVectorT(v3);
 
   std::cout << "------------erase" << '\n';
   std::vector<int> v4{0, 1, 2, 3, 5, 6};
   printVectorT(v4);
   auto i4 = v4.erase(begin(v4));
-  std::cout << *i4 <<'\n';
+  std::cout << *i4 << '\n';
   i4 = v4.erase(begin(v4) + 1);
-  std::cout << *i4 <<'\n';
+  std::cout << *i4 << '\n';
   std::cout << "------------emplace" << '\n';
   // emplace, make a new element with forwarded counstructor args
-  std::vector<p2d> p2dv {p2d(1,2)};
-  p2dv.emplace_back(9,7);
-  p2dv.emplace(begin(p2dv), 88,88);
-  for(auto i : p2dv){
-    std::cout << "(" << i.x << " " << i.y  << ")";
+  std::vector<p2d> p2dv{p2d(1, 2)};
+  p2dv.emplace_back(9, 7);
+  p2dv.emplace(begin(p2dv), 88, 88);
+  for (auto i : p2dv) {
+    std::cout << "(" << i.x << " " << i.y << ")";
   }
   print("\n");
 
   print("-----------swap");
   std::vector<int> v5{0, 1, 2, 3, 5, 6};
-  std::vector<int> v6{70, 71, 72, 73 };
-  std::swap(v5,v6);
+  std::vector<int> v6{70, 71, 72, 73};
+  std::swap(v5, v6);
   printVectorT(v5);
   printVectorT(v6);
 
   print("-----------value initialization");
-  std::vector<int> v7 (4);
-  print(v7.size(), v7.capacity());  // 4, 4
-  printVectorT(v7);// all initialize to 0
-  std::vector<p2d> v8 (2);  // call default ctor to initialize all elements in vector
+  std::vector<int> v7(4);
+  print(v7.size(), v7.capacity()); // 4, 4
+  printVectorT(v7);                // all initialize to 0
+  std::vector<p2d> v8(
+      2); // call default ctor to initialize all elements in vector
   print("-----------p2d");
   printp2dv(v8);
 }
 
-void testEnum(){
+void testEnum() {
   // prefer scoped enum(with class)
-  enum class week{mon, tue, wed,thu,fri,sat,sun};
-  int a = static_cast<int> ( week::mon );
+  enum class week { mon, tue, wed, thu, fri, sat, sun };
+  int a = static_cast<int>(week::mon);
   print(a);
   // int i = 3;
   // week w1 = static_cast<week> (i);
 }
 
-void testString(){
-  std::string path = R"(C:\Users\Name\Documents\file.txt)";  // No need to escape backslashes
+void testString() {
+  std::string path =
+      R"(C:\Users\Name\Documents\file.txt)"; // No need to escape backslashes
   print(path);
   std::string poem = R"(Roses are red,
 Violets are blue,
@@ -136,10 +145,58 @@ And so are you.)";
   std::string complex = R"xyz(Text with )" inside)xyz";
   print(complex);
 }
+
+void foo(Counter const &c) {
+  print("in foo");
+  c.print();
+}
+
+void testPtr(){
+  // c++11, no make_unique
+  // std::unique_ptr<Counter> pct(new Counter(9,"counter1"));
+  // or brace
+  std::unique_ptr<Counter> pct(new Counter{9,"counter1"});
+  pct->print();
+
+  auto pct2 = std::make_unique<Counter>(99, "counter2");
+  pct2->print();
+
+  auto ptr = std::make_unique<int>(42);
+  std::cout << *ptr << std::endl;
+  *ptr = 420;
+  print(*ptr);
+
+  // auto p2 = ptr;  unique_ptr is not copyable
+  auto p3 = std::move(ptr);  // unique_ptr is moveable
+  print(*p3);
+  // print(*ptr);  // ptr was moved, this cause segmentation fault
+}
+
+void testCls() {
+  Counter ct1{2};
+  ct1.print();
+  // if the ctor defined in Counter is explicit, the following statement trigger error
+  // no implicit conversion
+  // foo(3);
+  foo(Counter(3));
+}
+
+void testThrow(){
+  try{
+    int d = 1;
+    std::cin >> d;
+    Fraction f(1,d);
+  }catch(std::invalid_argument const& e){
+    std::cerr <<"error: " << e.what() << '\n';
+  }
+}
 int main() {
   std::cout << "hello\n";
   // testlimit();
-  // testvector();
+  testvector();
   // testEnum();
-  testString();
+  // testString();
+  // testCls();
+  // testPtr();
+  // testThrow();
 }
--- json/main.cpp
+++ json/main.cpp
@@ -2,6 +2,7 @@
 
 using json = nlohmann::json;
 
+
 auto main(int argc, char *argv[]) -> int {
     (void)argc; (void)argv;   // Explicitly mark as unused if needed
     std::ifstream f("example.json");
@@ -9,7 +10,7 @@ auto main(int argc, char *argv[]) -> int {
 
     print(data["name"]);
     print(data["data"]);
-    print(data["data"]["users"]["id"]);
+    // print(data["data"]["users"]["id"]);
 
     return 0;
 }
